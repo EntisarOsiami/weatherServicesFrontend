d43783c958a86a2f6b2b0cc72ab2234bd06d7e1f
entisarosaimi <code.g.light@gmail.com>
2025-06-23 23:08:26 +0300

    update the url to use the weatherServiceUrl

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosResponse } from 'axios'
 
 export const apiClient = axios.create({
-  baseURL: 'https://weatherapitypescript.onrender.com',
+  baseURL: 'https://weatherservices.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
